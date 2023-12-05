1e21e3f908a196e13c10f88debbb54e4df54c2de
Martin Accou <martin.accou@icloud.com>
2023-12-05 19:12:46 +0100

    5 day 1 exo

---
+++ puzzle5-1.py
@@ -0,0 +1,39 @@
+with open("data/puzzle5.csv") as file:
+    line = file.readline()
+    seeds = [int(x) for x in line.split(":")[1].split()]
+
+    mappings = []
+    line = file.readline()
+    line = file.readline()
+    line = file.readline()
+    for i in range(7):
+        mapping = []
+        while line.split():
+            mapping_item = line.split()
+            mapping_item = [int(x) for x in mapping_item]
+            mapping.append(mapping_item)
+            line = file.readline()
+        mappings.append(mapping)
+        line = file.readline()
+        line = file.readline()
+
+    # print(mappings)
+    lowest = float("inf")
+
+    for seed in seeds:
+        current = seed
+        for i in range(len(mappings)):
+            for j in range(len(mappings[i])):
+                if not (
+                    mappings[i][j][1]
+                    <= current
+                    <= mappings[i][j][1] + mappings[i][j][2]
+                ):
+                    continue
+                else:
+                    current = mappings[i][j][0] + current - mappings[i][j][1]
+                    break
+        if current < lowest:
+            lowest = current
+            print(lowest)
+    print(lowest)
